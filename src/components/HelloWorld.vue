22a76151e963a02a653e6ca7f0fad5481c364adc
quecp <quecp@pd02.com>
2023-08-17 12:11:58 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<!-- @format -->
+
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ElTable } from 'element-plus';
+
+interface Props {
+  msg: string;
+}
+
+withDefaults(defineProps<Props>(), {});
+
+const tableRef = ref<InstanceType<typeof ElTable>>();
+
+defineExpose({ tableRef });
+</script>
+
+<style scoped></style>
